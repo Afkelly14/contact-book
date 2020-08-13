46c55671ee9aaa99a6c41771567ada1ee28a24a6
Alex_Kelly <afkelly14@Gmail.com>
2020-08-13 10:20:06 -0400

    added value to table

--- lib/contacts.py
+++ lib/contacts.py
@@ -1,6 +1,23 @@
 from peewee import *
+from datetime import date
 
 db = PostgresqlDatabase('contacts', user='postgres', password='',
                         host='localhost', port=5432)
 
 db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Person(BaseModel):
+    first_name = CharField()
+    last_name = CharField()
+    birthday = DateField()
+    number = CharField()
+
+
+db.create_tables([Person])
+
+alex = Person(first_name='Alex', last_name='Kelly', birthday=date(1992, 6, 10), number='571-215-0235')
+alex.save()
